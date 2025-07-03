0c900ded2860d6c7c842961680592aa8462ebaac
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-07-03 13:15:02 +0300

    Update App

--- src/App.jsx
+++ src/App.jsx
@@ -8,69 +8,82 @@ import ProductDetails from "./pages/ProductDetails";
 import NotFound from "./pages/NotFound";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Layout from "./components/Layout";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <>
-        <Header />
-        <Home />
-        <Footer/>
-      </>
-    ),
-  },
-  {
-    path: "about",
-    element: (
-      <>
-        <Header />
-        <About />
-        <Footer/>
-      </>
-    ),
-  },
-  {
-    path: "cart",
-    element: (
-      <>
-        <Header />
-        <Cart />
-        <Footer/>
-      </>
-    ),
-  },
-  {
-    path: "categories",
-    element: (
-      <>
-        <Header />
-        <Categories />
-        <Footer/>
-      </>
-    ),
-  },
-  {
-    path: "product",
-    element: (
-      <>
-        <Header />
-        <ProductDetails />
-        <Footer/>
-      </>
-    ),
-  },
-  {
-    path: "*",
-    element: (
-      <>
-        <Header />
-        <NotFound />
-        <Footer/>
-      </>
-    ),
-  }
-]);
+const router = createBrowserRouter([{
+  path: "/", element: <Layout />, children: [
+    { path: "", element: <Home /> },
+    { path: "about", element: <About /> },
+    { path: "cart", element: <Cart /> },
+    { path: "categories", element: <Categories /> },
+    { path: "product", element: <ProductDetails /> },
+    { path: "*", element: <NotFound /> }
+  ]
+},
+])
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: (
+//       <>
+//         <Header />
+//         <Home />
+//         <Footer/>
+//       </>
+//     ),
+//   },
+//   {
+//     path: "about",
+//     element: (
+//       <>
+//         <Header />
+//         <About />
+//         <Footer/>
+//       </>
+//     ),
+//   },
+//   {
+//     path: "cart",
+//     element: (
+//       <>
+//         <Header />
+//         <Cart />
+//         <Footer/>
+//       </>
+//     ),
+//   },
+//   {
+//     path: "categories",
+//     element: (
+//       <>
+//         <Header />
+//         <Categories />
+//         <Footer/>
+//       </>
+//     ),
+//   },
+//   {
+//     path: "product",
+//     element: (
+//       <>
+//         <Header />
+//         <ProductDetails />
+//         <Footer/>
+//       </>
+//     ),
+//   },
+//   {
+//     path: "*",
+//     element: (
+//       <>
+//         <Header />
+//         <NotFound />
+//         <Footer/>
+//       </>
+//     ),
+//   }
+// ]);
 
 function App() {
   return <RouterProvider router={router} />;
